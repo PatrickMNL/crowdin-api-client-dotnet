db9a2ca6439128cef0d8d48599ae422c9f31aa2d
Patrick Leduc <patrick.leduc@miniclip.com>
2023-07-18 14:22:06 +0200

    fix: CheckBundleStatus - fix for in_progress not correctly mapped.

--- src/Crowdin.Api/BuildStatus.cs
+++ src/Crowdin.Api/BuildStatus.cs
@@ -1,4 +1,4 @@
-﻿
+
 using System.ComponentModel;
 using JetBrains.Annotations;
 
@@ -19,6 +19,28 @@ namespace Crowdin.Api
         [Description("failed")]
         Failed,
         
+        [Description("canceled")]
+        Canceled
+    }
+}
+
+namespace Crowdin.Api.Bundles
+{
+    [PublicAPI]
+    public enum BuildStatus
+    {
+        [Description("created")]
+        Created,
+
+        [Description("in_progress")]
+        InProgress,
+
+        [Description("finished")]
+        Finished,
+
+        [Description("failed")]
+        Failed,
+
         [Description("canceled")]
         Canceled
     }
